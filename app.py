2cc4561ea8952141a5d32cc56698b17c626f639a
FahmidaAhad <ahad.fahmida26@gmail.com>
2022-10-10 08:22:05 +0100

    consolidate pokemon data endpoints into a single function

--- app.py
+++ app.py
@@ -3,35 +3,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+pokemon_creatures = {
+    "bulbasaur": "dinosaur",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole"
+}
+
 
 @app.get("/")
 def pokemon_list():
     return 'Bulbosaur, charmander, pikachu, eevee, diglett'
 
 
-@app.get("/bulbasaur")
-def bulbasaur_data():
-    return "This is bulbasaur, a generation 1 pokemon who looks like a little dinosaur"
+@app.get("/<pokemon_name>")
+def pokemon_data(pokemon_name):
+    creature = pokemon_creatures.get(pokemon_name)
+    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
 
 
-@app.get("/charmander")
-def charmader_data():
-    return "This is charmander, a generation 1 pokemon who looks like a little reptile"
-
-
-@app.get("/pikachu")
-def pikachu_data():
-    return "This is pikachu, a generation 1 pokemon who looks like a tiny rodent"
-
-
-@app.get("/eevee")
-def eevee_data():
-    return "This is eevee, a generation 1 pokemon who looks like a tiny fox"
-
-
-@app.get("/diglett")
-def diglett_data():
-    return "This is diglett, a generation 1 pokemon who looks like a tiny mole"
+# if you want to insert a variable type argument then put it inside <> brackets
 
 
 if __name__ == '__main__':
